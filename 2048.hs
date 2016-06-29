d0ee1f248de7994f8da1599ca3efe366a3164629
David Eisinger <david.eisinger@viget.com>
2016-06-29 19:04:29 -0400

    Complete gameplay functionality

--- 2048.hs
+++ 2048.hs
@@ -1,3 +1,5 @@
+import System.Random
+
 data Tile = Val Int | Empty deriving (Show, Eq)
 
 data Move = L | R | U | D
@@ -47,12 +49,51 @@ victory ([] : rs) = victory rs
 victory ((Val 2048 : _) : _) = True
 victory ((t : ts) : rs) = victory (ts : rs)
 
-addRandomTile :: [[Tile]] -> [[Tile]]
-addRandomTile = undefined
+emptyTiles :: [[Tile]] -> Int -> Int -> [(Int, Int)]
+emptyTiles [] _ _  = []
+emptyTiles ([] : rs) r c = emptyTiles rs (r + 1) 0
+emptyTiles ((Empty : ts) : rs) r c = (r, c) : emptyTiles (ts : rs) r (c + 1)
+emptyTiles ((_ : ts) : rs) r c = emptyTiles (ts : rs) r (c + 1)
 
-board :: [[Tile]]
-board = [ [Empty, Empty, Empty, Empty],
-          [Empty, Empty, Empty, Empty],
-          [Val 2, Empty, Empty, Val 2],
-          [Empty, Empty, Empty, Empty] ]
+setTile :: [[Tile]] -> Int -> Int -> Int -> [[Tile]]
+setTile [] _ _ _ = []
+setTile ((t : ts) : rs) 0 0 v = ((Val v) : ts) : rs
+setTile ((t : ts) : rs) 0 c v = let (r : _) = setTile [ts] 0 (c - 1) v in
+                                (t : r) : rs
+setTile (ts : rs) r c v = ts : setTile rs (r - 1) c v
 
+getNextMove :: [[Tile]] -> Move
+getNextMove board = if canMove R board then R
+                    else if canMove D board then D
+                    else if canMove L board then L
+                    else U
+
+addRandomTile :: [[Tile]] -> IO [[Tile]]
+addRandomTile board = do
+  let empties = emptyTiles board 0 0
+  idx <- randomRIO (0, (length empties - 1))
+  let (r, c) = empties !! idx
+  rv <- randomRIO (1 :: Int, 10)
+  let v = if rv == 10 then 4 else 2
+  return (setTile board r c v)
+
+iter :: [[Tile]] -> IO ()
+iter board = do
+  putStrLn (show board)
+  newState <- addRandomTile (applyMove (getNextMove board) board)
+
+  if victory newState then putStrLn ("Victory! " ++ show newState)
+  else if gameOver newState then putStrLn ("Failure! " ++ show newState)
+  else iter newState
+
+main :: IO ()
+main = do
+  let board = [ [Empty, Empty, Empty, Empty],
+                [Empty, Empty, Empty, Empty],
+                [Empty, Empty, Empty, Empty],
+                [Empty, Empty, Empty, Empty] ]
+
+  board <- addRandomTile board
+  board <- addRandomTile board
+
+  iter board
