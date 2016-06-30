724ec08f583b59398ed2e07edef02f84dca85297
David Eisinger <david.eisinger@viget.com>
2016-06-30 16:14:43 -0400

    Gradient algorithm

--- Algorithm.hs
+++ Algorithm.hs
@@ -1,9 +1,22 @@
 module Algorithm where
 
 import Game
+import Data.List
+
+tileScore :: Tile -> Int -> Int -> Int
+tileScore Empty _ _ = 0
+tileScore (Val n) r c = (n ^ 3) * (r + 1) * (c + 1)
+
+rowScore :: [Tile] -> Int -> Int
+rowScore ts r = sum (map (\c -> tileScore (ts !! c) r c) [0..3])
+
+score :: Board -> Int
+score board = sum (map (\r -> rowScore (board !! r) r) [0..3])
+
+moveScore :: Move -> Board -> Int
+moveScore m board = score (applyMove m board)
 
 getNextMove :: Board -> Move
-getNextMove board = if canMove R board then R
-                    else if canMove D board then D
-                    else if canMove L board then L
-                    else U
+getNextMove board =
+  let comparison = \m1 m2 -> compare (moveScore m1 board) (moveScore m2 board) in
+  maximumBy comparison (possibleMoves board)
--- notes.md
+++ notes.md
@@ -4,3 +4,8 @@
     AVG. SCORE: 446
     WINS:       0
 
+## Gradient
+
+    AVG. TURNS: 303
+    AVG. SCORE: 671
+    WINS:       0
