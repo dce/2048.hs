cebe87ad4dcc582f4f81dffaaded773fec4b51a4
David Eisinger <david.eisinger@viget.com>
2016-06-30 17:09:52 -0400

    Gradient w/ lookahead

--- Algorithm.hs
+++ Algorithm.hs
@@ -3,6 +3,11 @@ module Algorithm where
 import Game
 import Data.List
 
+maxMoveBy :: (Move -> Board -> Int) -> Board -> Move
+maxMoveBy f board =
+  let comparison = (\m1 m2 -> compare (f m1 board) (f m2 board)) in
+  maximumBy comparison (possibleMoves board)
+
 tileScore :: Tile -> Int -> Int -> Int
 tileScore Empty _ _ = 0
 tileScore (Val n) r c = (n ^ 3) * (r + 1) * (c + 1)
@@ -16,7 +21,11 @@ score board = sum (map (\r -> rowScore (board !! r) r) [0..3])
 moveScore :: Move -> Board -> Int
 moveScore m board = score (applyMove m board)
 
+bestNextScore :: Move -> Board -> Int
+bestNextScore m board =
+  let nextBoard = applyMove m board in
+  let nextMove  = maxMoveBy moveScore nextBoard in
+  score (applyMove nextMove nextBoard)
+
 getNextMove :: Board -> Move
-getNextMove board =
-  let comparison = \m1 m2 -> compare (moveScore m1 board) (moveScore m2 board) in
-  maximumBy comparison (possibleMoves board)
+getNextMove board = maxMoveBy bestNextScore board
